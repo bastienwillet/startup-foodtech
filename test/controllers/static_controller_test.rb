9aac193fe50d100bf5b82aad2a05c77d6c6b2c4d
Bastien Willet <40465550+bastienwillet@users.noreply.github.com>
2018-08-22 10:42:45 +0200

    Update static_controller_test.rb

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Accès accueil" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Kebab Land"
+
+  end
 end
